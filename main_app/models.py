ecb5ccf5b30811b0c904ed11ba76671545d3768e
Aaron Goldsmith <aarongoldsmith1@outlook.com>
2021-03-16 10:02:14 -0700

    add event model, detail page

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
 
+EVENT_TYPES = (
+    ('T', 'Technology'),
+    ('M', 'Marketing'),
+    ('N', 'Networking'),
+    ('C', 'Career')
+)
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=250)
+    dateTime = models.DateTimeField()
+    speaker = models.CharField(max_length=250)
+    description = models.CharField(max_length=350)
+    type = models.CharField(
+        max_length=1,
+        choices=EVENT_TYPES,
+        default=EVENT_TYPES[0][0]
+    )
+    users = models.ManyToManyField(User, blank=True)
