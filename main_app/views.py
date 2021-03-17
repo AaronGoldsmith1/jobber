0440d6261d045e64fe46c74c1675e5acdf3f1fe2
ivwu22 <ivwu22@gmail.com>
2021-03-17 18:07:19 -0400

    adds carousel

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, password_validation
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.validators import UnicodeUsernameValidator
