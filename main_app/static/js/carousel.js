f284fc54a6c02cdf2172e9913bf2f6b548bddec7
Aaron Goldsmith <aarongoldsmith1@outlook.com>
2021-03-18 10:39:36 -0700

    style homepage carousel

--- main_app/static/js/carousel.js
+++ main_app/static/js/carousel.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
     $('.event-cards').slick({
         slidesToShow:5,
         slidesToScroll:5,
-        prevArrow: '<button class="slick-arrow slick-prev"></button>',
-        nextArrow: '<button class="slick-arrow slick-next"></button>'
+        arrows: true,
+        prevArrow:"<i class='far fa-caret-circle-left fa-3x slick-prev'></i>",
+        nextArrow:"<i class='far fa-caret-circle-right fa-3x slick-next'></i>"
     });
 });
