a946d7b78f2852a86598465ae0ab947ceb20fbc1
Aaron Goldsmith <aarongoldsmith1@outlook.com>
2021-03-18 20:23:54 -0700

    fix carousel

--- main_app/static/js/carousel.js
+++ main_app/static/js/carousel.js
@@ -1,9 +1,13 @@
+console.log('ready')
+
 $(document).ready(function(){
-    $('.event-cards').slick({
-        slidesToShow:5,
-        slidesToScroll:5,
-        arrows: true,
-        prevArrow:"<i class='far fa-caret-circle-left fa-3x slick-prev'></i>",
-        nextArrow:"<i class='far fa-caret-circle-right fa-3x slick-next'></i>"
-    });
+    if ($('.event-cards .card').length > 5) {
+        $('.event-cards').slick({
+            slidesToShow:5,
+            slidesToScroll:5,
+            arrows: true,
+            prevArrow:"<i class='far fa-caret-circle-left fa-3x slick-prev'></i>",
+            nextArrow:"<i class='far fa-caret-circle-right fa-3x slick-next'></i>"
+        });
+    }
 });
