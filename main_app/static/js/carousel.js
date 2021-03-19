f2bc449e3a98b8c45f2fd3b28447f411f66bafbb
Aaron Goldsmith <aarongoldsmith1@outlook.com>
2021-03-18 20:38:04 -0700

    add responsive carousel

--- main_app/static/js/carousel.js
+++ main_app/static/js/carousel.js
@@ -7,7 +7,35 @@ $(document).ready(function(){
             slidesToScroll:5,
             arrows: true,
             prevArrow:"<i class='far fa-caret-circle-left fa-3x slick-prev'></i>",
-            nextArrow:"<i class='far fa-caret-circle-right fa-3x slick-next'></i>"
+            nextArrow:"<i class='far fa-caret-circle-right fa-3x slick-next'></i>",
+             responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
         });
     }
 });
