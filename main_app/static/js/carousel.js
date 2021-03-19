207a3e2c363316c371889f120b9017ffd460415d
Aaron Goldsmith <aarongoldsmith1@outlook.com>
2021-03-18 20:39:36 -0700

    remove footer, carousel breakpoints

--- main_app/static/js/carousel.js
+++ main_app/static/js/carousel.js
@@ -5,7 +5,32 @@ $(document).ready(function(){
             slidesToScroll:5,
             arrows: true,
             prevArrow:"<i class='far fa-caret-circle-left fa-3x slick-prev'></i>",
-            nextArrow:"<i class='far fa-caret-circle-right fa-3x slick-next'></i>"
+            nextArrow:"<i class='far fa-caret-circle-right fa-3x slick-next'></i>",
+            responsive: [
+                {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+                },
+                {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+                },
+                {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+                }
+            ]
         });
     }   
 });
